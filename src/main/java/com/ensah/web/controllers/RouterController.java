927f1c5148f0b8844b536d2390d988f14293c427
Redouan El khanoussi <63659937+Redouan-El-Khanoussi@users.noreply.github.com>
2021-10-27 11:04:31 +0000

    updating

--- src/main/java/com/ensah/web/controllers/RouterController.java
+++ src/main/java/com/ensah/web/controllers/RouterController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class AmineRouterController {
+public class RouterController {
 	
 	@GetMapping("/justification/add")
 	public String addJustification() {
